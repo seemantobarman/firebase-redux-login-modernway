43768720866ae2a5b9276ccb5ee6238a0cab53fb
Seemanto Barman <seemanto@hotmail.com>
2021-05-07 14:22:53 +0600

    middleware added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
+//Middleware
+const logger = (store) => (next) => (action) => {
+	console.log("Dispatching--->", action);
+	console.log("State Before--->", store.getState());
+	next(action);
+	console.log("State After--->", store.getState());
+};
+
 export default configureStore({
 	reducer: {
 		user: userReducer,
 	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
